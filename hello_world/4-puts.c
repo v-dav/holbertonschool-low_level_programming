cc310c6be5777f9c91d2ee24a8edd5b00e263c4c
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-02-17 05:35:21 -0800

    added a desciption of the function and parentheses around return value

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * Function printing "Programming is like building a multilingual puzzle, followed by a new line.
+ * Puts() function with a string in it
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return(0);
 }
