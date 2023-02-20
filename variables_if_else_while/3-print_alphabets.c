da5e6ef7ce98429ce0a30be1ef46add883ee40fb
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-02-20 05:33:24 -0800

    2-print_alphabet description correction & 3-print_alphabet.c creation: a program that prints the alphabet in lowercase, and then in uppercase, followed by a new line.

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -3,18 +3,27 @@
 /**
  * main - Entry point and loops through letters to make an alphabet suite
  *
- * Description: prints the alphabet in lowercase followed by a new line.
- * Example: abcdefghijklmnopqrstuvwxyz
+ * Description: prints the alphabet in lowercase, and then in uppercase,
+ * followed by a new line.
+ * Example: abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
  * Return: 0 succes program exit
  */
 int main(void)
 {
 	char letter = 'a';
+	char LETTER = 'A';
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		putchar(letter);
 	}
+
+	for (LETTER = 'A'; LETTER <= 'Z'; LETTER++)
+	{
+		putchar(LETTER);
+	}
+
 	putchar('\n');
+
 	return (0);
 }
