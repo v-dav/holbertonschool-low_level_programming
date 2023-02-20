f85f18d56af2d0475fb45ea733f02ee0b6cd5909
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-02-20 08:29:40 -0800

    a program that prints all possible combinations of single-digit numbers. Loop correction

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -3,23 +3,30 @@
 /**
  * main - Entry point
  *
- * Description: prints all the numbers of base 16 in lowercase
- * followed by a new line
- * Example: 0123456789abcdef
+ * Description: a program that prints all possible combinations
+ * of single-digit numbers separated by comma and space,
+ * except after the digit 9 followed by a new line.
+ * Example: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
  * Return: 0 success, exit
  */
 int main(void)
 {
 	int i = 48;
 
-	for (i = 48; i < 57; i++)
+	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
+
+		if (i < 57)
+		{
 		putchar(44);
 		putchar(32);
+		}
+		else
+		{
+		putchar(10);
+		}
 	}
-
-	putchar(57);
 
 	return (0);
 }
