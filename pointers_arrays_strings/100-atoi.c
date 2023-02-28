f8cffac2537d307443e9d120199ad37d2fb736cb
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-02-28 05:46:35 -0800

    a function that convert a string to an integer.

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,48 +8,34 @@
  */
 int _atoi(char *s)
 {
-	int t = 0;
-	int j = 0;
-	int sign = 1;
-	int resultat = 0;
-	int nb_trouve = 0;
+	int t = 0, j = 0, sign = 1, resultat = 0, nb_trouve = 0;
 
 	while (s[t] != '\0')
-	{
 		t++;
-	}
 	t--;
 	while (j <= t)
 	{
-        	if (s[j] == '-')
-        	{
-            		sign *= -1;
-        	}
-        
-        	if (s[j] >= 48 && s[j] <= 57) 
-        	{
-            		resultat = resultat * 10 + ((int) s[j] - 48);
-            		nb_trouve = 1;
-        	}
-        	else
-        	{
-            		if (nb_trouve == 1)
-            		break;
-        	}
-        	j++;
-    	}
-
-	if (resultat > 0) 
-    	{
-        	if (sign == -1)
-        	{    
-            		return (resultat * (-1));
-        	}
-        	else
-        	{
-            		return (resultat);
-        	}
-    	}
-    	else
-        	return (0);
+		if (s[j] == '-')
+			sign *= -1;
+		if (s[j] >= 48 && s[j] <= 57)
+		{
+			resultat = resultat * 10 + ((int) s[j] - 48);
+			nb_trouve = 1;
+		}
+		else
+		{
+			if (nb_trouve == 1)
+				break;
+		}
+		j++;
+	}
+	if (resultat > 0)
+	{
+		if (sign == -1)
+			return (resultat * (-1));
+		else
+			return (resultat);
+	}
+	else
+		return (0);
 }
