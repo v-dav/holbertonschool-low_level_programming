001f76ff0776547dd55dee095f7e6383c4d6f6a0
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-03-02 03:46:30 -0800

    a function that fills memory with a constant byte.

--- pointers_arrays_strings/main.c
+++ pointers_arrays_strings/main.c
@@ -2,17 +2,45 @@
 #include <stdio.h>
 
 /**
- * main - check the code for
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+        unsigned int i;
+
+        i = 0;
+        while (i < size)
+        {
+                if (i % 10)
+                {
+                        printf(" ");
+                }
+                if (!(i % 10) && i)
+                {
+                        printf("\n");
+                }
+                printf("0x%02x", buffer[i]);
+                i++;
+        }
+        printf("\n");
+}
+
+/**
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
-    char *p;
+    char buffer[98] = {0x00};
 
-    p = leet(s);
-    printf("%s", p);
-    printf("%s", s);
+    simple_print_buffer(buffer, 98);
+    _memset(buffer, 0x01, 95);
+    printf("-------------------------------------------------\n");
+    simple_print_buffer(buffer, 98);    
     return (0);
 }
