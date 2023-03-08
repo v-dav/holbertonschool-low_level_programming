75b3b2b1f1923a35d02995d2e87497bca1813f9b
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-03-08 07:16:22 -0800

    corrected the function

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -12,7 +12,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int sum, i;
+	int sum = 0, i, j;
 
 	if (argc == 1)
 	{
@@ -22,10 +22,13 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		if (!isdigit(*argv[i]))
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			printf("Error\n");
-			return (1);
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
 		sum += atoi(argv[i]);
 	}
