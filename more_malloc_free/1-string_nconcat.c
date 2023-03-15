d539057cf8584d8c45afbcef6d695f58eb8ec815
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-03-15 06:53:04 -0700

    function corrected

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -12,7 +12,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int s1_size = 0;
 	unsigned int s2_size = 0;
-	char *s_concat;
+	char *s_concat = NULL;
 	unsigned int i;
 
 	if (s1 != NULL)
@@ -20,29 +20,27 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		while (s1[s1_size])
 			s1_size++;
 	}
+	else
+		s1 = "";
 	if (s2 != NULL)
 	{
 		while (s2[s2_size])
 			s2_size++;
 	}
-	if (n > 0 && n < s2_size && s2 != NULL)
-		s_concat = malloc(s1_size + n + 1);
-	else if (n > 0 && n >= s2_size && s2 != NULL)
-		s_concat = malloc(s1_size + s2_size + 1);
-	else if ((n == 0 || s2 == NULL) && s1 != NULL)
-		s_concat = malloc(s1_size + 1);
+	else
+		s2 = "";
+	if (n >= s2_size)
+		n = s2_size;
+	s_concat = malloc(s1_size + n + 1);
 	if (s_concat == 0)
 		return (0);
-	if (s1 != NULL)
-	{
-		for (i = 0; i < s1_size; i++)
-			s_concat[i] = s1[i];
-	}
-	if (s2 != NULL && n > 0)
-	{
-		for (i = 0; i < n && s2[i] != 0; i++)
-			s_concat[s1_size + i] = s2[i];
-	}
+	for (i = 0; i < s1_size; i++)
+		s_concat[i] = s1[i];
+
+	for (i = 0; i < n; i++)
+		s_concat[s1_size + i] = s2[i];
+
+	s_concat[s1_size + i] = '\0';
 	return (s_concat);
 }
 
