ac137dd2c6c27ea0a203cad45f1852f1bcaf3514
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-03-15 04:51:55 -0700

    modified mistake

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -13,7 +13,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int s1_size = 0;
 	unsigned int s2_size = 0;
 	char *s_concat;
-	unsigned i;
+	unsigned int i;
 
 	if (s1 != NULL)
 	{
