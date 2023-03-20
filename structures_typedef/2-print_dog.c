0e7434997dcc06c92fe7fee0a879e9fa8eaca220
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-03-20 11:23:28 -0700

    corrected function for the case age 0

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -17,7 +17,7 @@ void print_dog(struct dog *d)
 		if (d->age != 0)
 			printf("Age: %f\n", d->age);
 		else
-			printf("Age: (nil)\n");
+			printf("Age: .0f\n");
 		if (d->owner != 0)
 			printf("Owner: %s\n", d->owner);
 		else
