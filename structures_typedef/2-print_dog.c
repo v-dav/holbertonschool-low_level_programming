770a741a7405298b9b3ec11973e67e09f19ef354
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-03-20 11:27:14 -0700

    another correction for null case in age

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -14,10 +14,9 @@ void print_dog(struct dog *d)
 			printf("Name: %s\n", d->name);
 		else
 			printf("Name: (nil)\n");
-		if (d->age != 0)
-			printf("Age: %f\n", d->age);
-		else
-			printf("Age: %f\n", 0);
+
+		printf("Age: %f\n", d->age);
+
 		if (d->owner != 0)
 			printf("Owner: %s\n", d->owner);
 		else
