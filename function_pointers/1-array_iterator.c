c3847501097ebba847ec8e54321ccd4946ef7d4d
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-03-21 13:16:13 -0700

    a function that executes a function given as a parameter on each element of an array: fixed case for NULL array or pointer as an argument

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -12,11 +12,16 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	long unsigned int i = 0;
-
-	while (i < size)
-	{
-		(*action)(array[i]);
-		i++;
+	unsigned long int i = 0;
+	
+	if (array == NULL || action == NULL)
+	{}
+	else
+	{	
+		while (i < size)
+		{
+			(*action)(array[i]);
+			i++;
+		}
 	}
 }
