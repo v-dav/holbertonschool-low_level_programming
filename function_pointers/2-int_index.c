13e3ef5b9797e90e2fed97516b262ea72fef110f
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-03-22 02:14:08 -0700

    fixed NULL cases

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -13,8 +13,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int result;
 	int i;
-
-	if (size <= 0)
+	
+	if (size <= 0 || array == 0 || cmp == 0)
 		return (-1);
 
 	for (i = 0; i < size; i++)
