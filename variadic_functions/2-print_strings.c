b6e479388b507fe0a3908340acaf4a5474e9cb11
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-03-23 12:43:15 -0700

    fixed NULL string case

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -15,11 +15,18 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list ap;
+	char *ptr;
 
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%s", va_arg(ap, char *));
+		ptr = va_arg(ap, char *);
+
+		if (ptr == NULL)
+			printf("%p", ptr);
+		else
+			printf("%s", ptr);
+
 		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
