2846eba73dabedfca2b3d5d4f9573d19db8c113d
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-03-24 08:00:16 -0700

    trying to fix NULL string case

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -71,7 +71,7 @@ void print_all(const char * const format, ...)
 
 	va_start(ap, format);
 
-	while (format[i] != '\0')
+	while (format != NULL && format[i] != '\0')
 	{
 		while (j < 4)
 		{
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -2,7 +2,6 @@
 #define VARIADIC_FUNCTIONS_H
 #include <stdio.h>
 #include <stdarg.h>
-#include <stdlib.h>
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
