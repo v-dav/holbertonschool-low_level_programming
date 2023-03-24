761dd997d5bb6425e0c07b3ef0dde062a0c009dc
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-03-24 07:51:46 -0700

    NULL case fixed

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,20 +1,42 @@
 #include "variadic_functions.h"
 
+/**
+ * print_char - a function that prints an char
+ * @ap: argument parameter
+ *
+ * Return: nothing
+ */
 void print_char(va_list ap)
 {
 	printf("%c", va_arg(ap, int));
 }
 
+/**
+ * print_int - a function that prints an integer
+ * @ap: argument parameter
+ *
+ * Return: Always 0.
+ */
 void print_int(va_list ap)
 {
 	printf("%d", va_arg(ap, int));
 }
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 void print_float(va_list ap)
 {
 	printf("%f", va_arg(ap, double));
 }
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 void print_string(va_list ap)
 {
 	char *s;
@@ -22,12 +44,14 @@ void print_string(va_list ap)
 	s = va_arg(ap, char*);
 	if (s == NULL)
 		s = "(nil)";
-	
 	printf("%s", s);
 }
 
-/*----------------------------------*/
-
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 void print_all(const char * const format, ...)
 {
 
