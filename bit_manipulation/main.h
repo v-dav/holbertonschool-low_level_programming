b1f36d5e0c33744791d3d2f72eee73d610bbd1a4
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-04-06 05:00:49 -0700

    added _pow_recursion to main.h header file

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 int _putchar(char c);
+int _pow_recursion(int x, int y);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
