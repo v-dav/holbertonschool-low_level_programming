31b9c5ef567b39c7350e962827d9496177197efd
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-04-07 05:50:19 -0700

    fixed case for number 2048 and index 1 and 20

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -18,8 +18,8 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	if (*n == 0)
 		return (1);
 
-	mask = one << index;
-	*n ^= mask;
+	mask = ~(one << index);
+	*n &= mask;
 
 	return (1);
 }
