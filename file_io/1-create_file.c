414253da86f7955c6eea9809d0bee17f7cdb6259
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-04-11 19:45:10 +0200

    fix betty style

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -30,8 +30,8 @@ int create_file(const char *filename, char *text_content)
 	written = write(fd, text_content, strlen(text_content));
 	if (written == -1)
 	{
-        close(fd);
-        return (-1);
+		close(fd);
+		return (-1);
 	}
 
 	close(fd);
