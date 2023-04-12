d758dd8ab41b3cc5ce9377db18f9261f1c5284c8
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-04-12 20:16:33 +0200

    fix betty style by dividing main into functions

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,6 +1,55 @@
 #include "main.h"
 
 /**
+ * create_buffer - a function that creates a buffer of specific size
+ * @buffer_size: a size of wanted buffer in bytes
+ *
+ * Return: the address of the created buffer in memory
+ */
+char *create_buffer(ssize_t buffer_size)
+{
+	char *buffer = malloc(sizeof(char) * buffer_size);
+
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Memory allocation failed\n");
+		exit(96);
+	}
+	return (buffer);
+}
+
+/**
+ * error_message - a function that displays an error message
+ * @msg: the error message to display
+ * @exit_code: the exit code
+ * @filename: the name of the file to display
+ *
+ * Return: void
+ */
+void error_message(char *msg, int exit_code, char *filename)
+{
+	dprintf(STDERR_FILENO, "%s %s\n", msg, filename);
+	exit(exit_code);
+}
+
+/**
+ * close_file - a function that closes an open file
+ * @fd: a file description that identifies the file
+ *
+ * Return: void
+ */
+void close_file(int fd)
+{
+	int result = close(fd);
+
+	if (result == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
  * main - a program that copies the content of a file to another file
  * @argc: number of arguments on command line (max 3)
  * @argv: array
@@ -12,36 +61,23 @@
  */
 int main(int argc, char **argv)
 {
-	int fd_src, fd_dest, close_src, close_dest, open_flags;
+	int fd_src, fd_dest, open_flags;
 	char *buffer, *write_buffer;
-	ssize_t bytes_read, written_to, buffer_size, bytes_to_write;
+	ssize_t bytes_read, written_to, bytes_to_write;
 	mode_t permissions;
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		error_message("Usage: cp file_from file_to", 97, "");
 
-	buffer_size = 1024;
-	buffer = malloc(sizeof(char) * buffer_size);
-	if (buffer == NULL)
-	{
-		dprintf(STDERR_FILENO, "Error: Memory allocation failed\n");
-		exit(96);
-	}
+	buffer = create_buffer(1024);
 	open_flags = O_WRONLY | O_CREAT | O_TRUNC;
 	permissions = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 	fd_src = open(argv[1], O_RDONLY);
 	fd_dest = open(argv[2], open_flags, permissions);
-
 	if (fd_src == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		error_message("Error: Can't read from file", 98, argv[1]);
 
-	while ((bytes_read = read(fd_src, buffer, buffer_size)) > 0)
+	while ((bytes_read = read(fd_src, buffer, 1024)) > 0)
 	{
 		bytes_to_write = bytes_read;
 		write_buffer = buffer;
@@ -51,34 +87,18 @@ int main(int argc, char **argv)
 			if (fd_dest == -1 || written_to == -1)
 			{
 				close(fd_src);
-				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-				exit(99);
+				error_message("Error: Can't write to", 99, argv[2]);
 			}
 			bytes_to_write -= written_to;
 			write_buffer += written_to;
 		}
 	}
-
 	if (bytes_read == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit (98);
-	}
+		error_message("Error: Can't read from file", 98, argv[1]);
 
-	close_src = close(fd_src);
-	close_dest = close(fd_dest);
-
-	if (close_src == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", close_src);
-		exit (100);
-	}
-	if (close_dest == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", close_dest);
-		exit (100);
-	}
-
+	close_file(fd_src);
+	close_file(fd_dest);
 	free(buffer);
+
 	return (0);
 }
