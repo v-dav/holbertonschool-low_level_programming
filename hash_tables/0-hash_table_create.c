68dd6bdb830bff3153a4c7987b775815cb484e19
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-04-13 15:35:58 +0200

    fix betty style and updated the size of the table

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -3,20 +3,19 @@
 /**
  * hash_table_create - a function that creates a hash table
  * @size: the size of the array
- * 
+ *
  * Return: the pointer to the newly created hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *ht = NULL;
+	hash_table_t *ht = NULL;
 
-    ht = malloc(sizeof(hash_table_t));
-    if (ht == NULL)
-        return(NULL);
+	ht = malloc(sizeof(hash_table_t) * size);
+	if (ht == NULL)
+		return (NULL);
 
-    ht->size = size;
-    ht->array = NULL;
+	ht->size = size;
+	ht->array = NULL;
 
-
-    return(ht);    
+	return (ht);
 }
