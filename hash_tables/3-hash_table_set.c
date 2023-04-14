a01ad75983b56e3e8498f64f57756a6173a1a7a3
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-04-14 11:06:23 +0200

    fix case where hash table is NULL

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -15,7 +15,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node, *old_node = NULL;
 	const char *old_value;
 
-	if (key == NULL)
+	if (key == NULL || ht == NULL)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 
