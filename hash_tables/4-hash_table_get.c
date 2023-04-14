fbed6f9015e59e9910ccddb9c1850287da555189
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-04-14 13:06:10 +0200

    fix function for manage collisions while searching

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -12,14 +12,19 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	int index;
+	hash_node_t *current_node = NULL;
 
 	if (key == NULL || ht == NULL)
 		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
+	current_node = ht->array[index];
 
-	if (ht->array[index] != NULL && strcmp(ht->array[index]->key, key) == 0)
-		return (ht->array[index]->value);
-	else
-		return (NULL);
+	while (current_node != NULL)
+	{
+		if (strcmp(current_node->key, key) == 0)
+			return (current_node->value);
+		current_node = current_node->next;
+	}
+	return (NULL);
 }
