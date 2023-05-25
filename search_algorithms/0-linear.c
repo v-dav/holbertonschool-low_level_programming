6209f50f3168b804c8abba86f97586571bb7226a
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-05-25 11:57:25 +0200

    fix printf formatting

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -3,13 +3,13 @@
 /**
  * linear_search - function that performs a linear search on an array to find
  * a specific value
- * 
+ *
  * @array: A pointer to the first element of the array to be searched
  * @size: represents the size of the array
  * @value:  is the integer value that we are searching for in the array
- * 
+ *
  * Return: the index of the first occurrence of the `value` in the `array`
- * if it is found, or `-1` if the `array` is `NULL` or the `value` is not 
+ * if it is found, or `-1` if the `array` is `NULL` or the `value` is not
  * found in the `array`
  */
 int linear_search(int *array, size_t size, int value)
@@ -21,7 +21,7 @@ int linear_search(int *array, size_t size, int value)
 
 	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array [%lu] = [%d]\n", i, array[i]);
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
 	}
